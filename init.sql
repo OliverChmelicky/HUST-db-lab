9d1768cf0b75e8f04f74b9886899659980db98dc
DuongDQ-194747 <102663176+DuongDQ-194747@users.noreply.github.com>
2022-07-23 13:18:21 +0700

    feat: add foreign_key constraints & indexes

--- init.sql
+++ init.sql
@@ -1,77 +1,90 @@
-CREATE TABLE IF NOT EXISTS public."Users"
-(
-    uid integer NOT NULL,
-    name character varying NOT NULL,
-    password character varying NOT NULL,
-    phonenum character varying NOT NULL,
-    address character varying NOT NULL,
-    PRIMARY KEY (uid)
+CREATE TABLE "Users" (
+  "id" bigserial PRIMARY KEY,
+  "name" varchar NOT NULL,
+  "password" varchar NOT NULL,
+  "phonenum" varchar NOT NULL,
+  "address" varchar NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS public."Orders"
-(
-    card_id integer NOT NULL,
-    user_id integer NOT NULL,
-    created_date timestamp without time zone NOT NULL,
-    total_price integer NOT NULL,
-    cart_status character varying NOT NULL,
-    shipping_address character varying NOT NULL,
-    shipping_fee integer NOT NULL,
-    voucher_id integer NOT NULL,
-    payment_info character varying NOT NULL,
-    PRIMARY KEY (card_id)
+CREATE TABLE "Orders" (
+  "id" bigserial PRIMARY KEY,
+  "user_id" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT 'now()',
+  "total_price" integer NOT NULL,
+  "cart_status" varchar NOT NULL,
+  "shipping_address" varchar NOT NULL,
+  "shipping_fee" integer NOT NULL,
+  "voucher_id" bigserial NOT NULL,
+  "payment_info" varchar NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS public."Vouchers"
-(
-    voucher_id integer NOT NULL,
-    condition integer,
-    value integer NOT NULL,
-    date_expire timestamp without time zone,
-    PRIMARY KEY (voucher_id)
+CREATE TABLE "Vouchers" (
+  "id" bigserial PRIMARY KEY,
+  "title" varchar NOT NULL,
+  "condition" integer NOT NULL DEFAULT 0,
+  "value" integer NOT NULL,
+  "date_expire" timestamptz NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS public."Products"
-(
-    product_id integer NOT NULL,
-    p_name character varying NOT NULL,
-    category_id integer NOT NULL,
-    description character varying,
-    status character varying,
-    price integer NOT NULL,
-    PRIMARY KEY (product_id)
+CREATE TABLE "Products" (
+  "id" bigserial PRIMARY KEY,
+  "name" varchar NOT NULL,
+  "category_id" integer NOT NULL,
+  "description" varchar,
+  "status" varchar NOT NULL DEFAULT 'PENDING',
+  "price" integer NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS public."ProductDetail"
-(
-    pd_id integer NOT NULL,
-    color character varying NOT NULL,
-    size character varying NOT NULL,
-    product_id integer NOT NULL,
-    PRIMARY KEY (pd_id)
+CREATE TABLE "Category" (
+  "id" bigserial PRIMARY KEY,
+  "title" varchar NOT NULL,
+  "description" varchar,
+  "parent_cate_id" bigint NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS public."Category"
-(
-    category_id integer NOT NULL,
-    title character varying NOT NULL,
-    description character varying,
-    parent_category integer,
-    PRIMARY KEY (category_id)
+CREATE TABLE "ProductStocks" (
+  "id" bigserial PRIMARY KEY,
+  "color" varchar NOT NULL,
+  "size" varchar NOT NULL,
+  "product_id" bigint NOT NULL,
+  "quantity_remain" integer NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS public."Stocks"
-(
-    pv_id integer NOT NULL,
-    quantity_remain integer NOT NULL,
-    PRIMARY KEY (pv_id)
+CREATE TABLE "ProductOrdereds" (
+  "id" bigserial PRIMARY KEY,
+  "cart_id" bigint NOT NULL,
+  "stock_id" bigint NOT NULL,
+  "quantity" integer NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS public."ProductOrdereds"
-(
-    po_id integer NOT NULL,
-    cart_id integer NOT NULL,
-    pv_id integer NOT NULL,
-    quantity integer NOT NULL,
-    PRIMARY KEY (po_id)
-);
+CREATE INDEX ON "Users" ("name");
+
+CREATE INDEX ON "Users" ("address");
+
+CREATE INDEX ON "Orders" ("user_id");
+
+CREATE INDEX ON "Orders" ("user_id", "cart_status");
+
+CREATE INDEX ON "Vouchers" ("title");
+
+CREATE INDEX ON "Products" ("name");
+
+CREATE INDEX ON "Category" ("title");
+
+CREATE INDEX ON "ProductStocks" ("product_id");
+
+CREATE INDEX ON "ProductOrdereds" ("cart_id");
+
+ALTER TABLE "Orders" ADD FOREIGN KEY ("user_id") REFERENCES "Users" ("id");
+
+ALTER TABLE "Orders" ADD FOREIGN KEY ("voucher_id") REFERENCES "Vouchers" ("id");
+
+ALTER TABLE "Products" ADD FOREIGN KEY ("category_id") REFERENCES "Category" ("id");
+
+ALTER TABLE "Category" ADD FOREIGN KEY ("parent_cate_id") REFERENCES "Category" ("id");
+
+ALTER TABLE "ProductStocks" ADD FOREIGN KEY ("product_id") REFERENCES "Products" ("id");
+
+ALTER TABLE "ProductOrdereds" ADD FOREIGN KEY ("cart_id") REFERENCES "Orders" ("id");
+
+ALTER TABLE "ProductOrdereds" ADD FOREIGN KEY ("stock_id") REFERENCES "ProductStocks" ("id");

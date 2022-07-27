4b4a79d2f6762c21a66c2f5eb0365fd2de86285c
DuongDQ-194747 <quangduongphp@gmail.com>
2022-07-27 09:39:39 +0700

    feat(db-script): add purchase_price in product_ordereds

--- database-scripts/1-init.sql
+++ database-scripts/1-init.sql
@@ -57,9 +57,9 @@ CREATE TABLE products (
 
 CREATE TABLE product_stocks (
     id bigserial PRIMARY KEY,
+    product_id bigint REFERENCES products (id) ON DELETE CASCADE,
     color varchar NOT NULL,
     size varchar NOT NULL,
-    product_id bigint REFERENCES products (id) ON DELETE CASCADE,
     quantity_remain integer NOT NULL
 );
 
@@ -67,18 +67,20 @@ CREATE TABLE orders (
     id bigserial PRIMARY KEY,
     user_id bigint NOT NULL REFERENCES users (id) ON DELETE CASCADE,			-- 
     created_at timestamptz NOT NULL DEFAULT 'now()',
-    total_price integer NOT NULL,
     status cart_status NOT NULL DEFAULT 'ToPay',
     shipping_address varchar NOT NULL,
     shipping_fee integer NOT NULL,
     voucher_id bigint NOT NULL REFERENCES vouchers (id) ON DELETE CASCADE,	--
+    total_price integer NOT NULL,
     payment_info varchar NOT NULL
 );
 
+
 CREATE TABLE product_ordereds (
     id bigserial PRIMARY KEY,
-    cart_id bigint NOT NULL REFERENCES orders (id) ON DELETE CASCADE,				--
-    stock_id bigint NOT NULL REFERENCES product_stocks (id)  ON DELETE CASCADE,	--
+    cart_id bigint NOT NULL REFERENCES orders (id) ON DELETE CASCADE,					--
+    stock_id bigint NOT NULL REFERENCES product_stocks (id)  ON DELETE CASCADE,			--
+    purchase_price int, 
     quantity integer NOT NULL
 );
 

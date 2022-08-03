b95c5077dae8200fe448e1f2479d3d231c85561e
olo <oliver.chmelicky@gmail.com>
2022-08-03 16:57:21 +0700

    feat(db-view): refactor

--- database-scripts/3-marterialized-view.sql
+++ database-scripts/3-marterialized-view.sql
@@ -1,6 +1,6 @@
-DROP MATERIALIZED VIEW IF EXISTS product_detail_mv CASCADE;
+DROP VIEW IF EXISTS product_detail_mv CASCADE;
 
-CREATE MATERIALIZED VIEW product_detail_mv 
+CREATE VIEW product_detail_mv
 AS
 	SELECT p.id,
 		c.title AS category,
@@ -12,16 +12,14 @@ AS
      	JOIN categories c ON ((c.id = p.category_id)))
      	FULL JOIN product_stocks s ON ((p.id = s.product_id)))
 	GROUP BY p.id, c.id
-	ORDER BY c.id
-WITH NO DATA;
-
-REFRESH MATERIALIZED VIEW product_detail_mv;
+	ORDER BY c.id;
 
 DROP VIEW IF EXISTS order_products CASCADE;
 
 CREATE VIEW order_products
-AS 
-	SELECT o.user_id,
+AS
+    SELECT
+        o.user_id,
         o.created_at,
         o.status,
         o.shipping_address,
@@ -29,14 +27,12 @@ AS
         po.quantity,
         ps.size,
         ps.color,
-        p.id,
         p.name,
         c.title
-	FROM orders o
-    JOIN product_ordereds po ON (o.id = po.cart_id)
-    JOIN product_stocks ps ON (po.stock_id = ps.id)
-    JOIN products p ON (ps.product_id = p.id)
-    JOIN categories c ON (p.category_id = c.id)
-    GROUP BY o.id, po.purchase_price, po.quantity,ps.size, ps.color, p.id, p.name, c.title
-    ORDER BY p.id
+        FROM orders o
+            JOIN product_ordereds po ON (o.id = po.cart_id)
+            JOIN product_stocks ps ON (po.stock_id = ps.id)
+            JOIN products p ON (ps.product_id = p.id)
+            JOIN categories c ON (p.category_id = c.id)
+        order by created_at;
 

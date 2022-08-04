6ab005b47090f739966bc08683428e4b1f65df97
DuongDQ-194747 <quangduongphp@gmail.com>
2022-08-04 13:25:36 +0700

    Revert "Merge branch 'main' of https://github.com/OliverChmelicky/HUST-db-lab"
    This reverts commit 945ab283d71a59dfb299f2c4cacceef3a6d1dc0c, reversing
    changes made to 356078ad1e1b15b6868c6633689ef34e549da05d.

--- database-scripts/2-user-priviledges.sql
+++ database-scripts/2-user-priviledges.sql
@@ -1,3 +1,29 @@
+-- -- testing structures
+-- CREATE TABLE IF NOT EXISTS public."testik"
+-- (
+--     uid varchar
+-- );
+--
+--
+-- CREATE OR REPLACE FUNCTION test_if_user_needs_rights_to_execute_procedure()
+--     RETURNS trigger AS
+-- $$
+-- BEGIN
+--     INSERT INTO testik (uid) VALUES (OLD.name);
+--     return OLD;
+-- END;
+-- $$
+--     LANGUAGE plpgsql;
+--
+--
+--
+-- CREATE TRIGGER test_if_user_needs_rights_trigger
+--     BEFORE DELETE ON "users"
+--     FOR EACH ROW
+-- EXECUTE PROCEDURE test_if_user_needs_rights_to_execute_procedure();
+
+
+-- REAL USER SCRIPT
 
 --BACKEND USER
 CREATE USER backend WITH  password 'backend';
